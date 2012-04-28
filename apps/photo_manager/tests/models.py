dc9df535d846568df28fe0c9717e472ac54dbd07
Derek Stegelman <dstegelman@gmail.com>
2012-04-28 17:44:24 -0500

    Browser tests for travis..

--- apps/photo_manager/tests/models.py
+++ apps/photo_manager/tests/models.py
@@ -5,8 +5,10 @@ from photo_manager.tests.factories import AlbumFactory, PhotoFactory
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-class ViewTest(TestCase):
-        
+
+class AlbumModelTest(TestCase):
+
+    
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username="derekst", email="bob@email.com", password="pass")
@@ -14,17 +16,14 @@ class ViewTest(TestCase):
         self.user.is_superuser = True
         self.user.save()
         self.album = AlbumFactory.create(user=self.user)
+        self.child_album = AlbumFactory.create(user=self.user, parent_album=self.album, title="Saint Peters")
+
+    def test_slugify(self):
+        self.assertEqual(self.album.slug, "roma")
         
-        
+    def test_child_albums(self):
+        self.assertTrue(self.album.has_child_albums)
+    
     def test_unicode(self):
         self.assertEqual(self.album.__unicode__(), "Roma")
-
-class AlbumModelTest(TestCase):
-
-    
-    def setUp(self):
-        self.client = Client()
-    
-    def test_quick(self):
-        self.assertEqual(1 + 1, 2)
             
