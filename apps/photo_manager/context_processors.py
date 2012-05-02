89999cf72aea1dead514cb609011b7a03b040aa2
Derek Stegelman <dstegelman@gmail.com>
2012-05-01 21:49:09 -0500

    Removing old code.

--- apps/photo_manager/context_processors.py
+++ apps/photo_manager/context_processors.py
@@ -1,19 +1,10 @@
 from locations.models import *
-from photo_manager.models import *
 from django.conf import settings
 
 def theme_files(request):
     context = {}
     
-    context['ENABLE_REGISTRATION'] = settings.ENABLE_REGISTRATION
     context['THEME_URL'] = "%sphoto_manager/themes/%s/" % (settings.STATIC_URL, settings.ACTIVE_THEME)
     
     return context
-
-def locations_albums(request):
-    context = {}
-    context['form_locations'] = Location.objects.all()
-    context['form_albums'] = Album.objects.all()
-    return context
-    
-    
+   
