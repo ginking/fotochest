1c6738e6f05814a7aaa5a2101d2bcd907578a2ee
Derek Stegelman <dstegelman@gmail.com>
2012-05-15 08:12:32 -0500

    Again manipulating the upload view.

--- apps/administrator/upload.py
+++ apps/administrator/upload.py
@@ -44,3 +44,48 @@ class JSONResponse(HttpResponse):
     def __init__(self,obj='',json_opts={},mimetype="application/json",*args,**kwargs):
         content = simplejson.dumps(obj,**json_opts)
         super(JSONResponse,self).__init__(content,mimetype,*args,**kwargs)
+        
+# Real Methods
+
+def upload_photo(request, location_slug, album_slug, user_id):
+    context = {}
+    if request.method == 'POST':
+        #
+        uploaded_file = request.FILES[u'files[]']
+        
+        # write the file into /tmp
+        num1 = str(random.randint(0, 1000000))
+        num2 = str(random.randint(1001, 9000000))
+        
+        ext = os.path.splitext(uploaded_file.name)[1]
+        filename = str(num1 + num2) + ext
+        
+        album_used = get_object_or_404(Album, slug=album_slug)
+        photo_new = Photo(title=filename, album=album_used)
+        photo_new.file_name = filename
+        photo_new.image = 'images/' + filename
+        # Set location to default location
+        photo_new.location = get_object_or_404(Location, slug=location_slug)
+        user = get_object_or_404(User, pk=user_id)
+        photo_new.user = user
+        photo_new.save()
+            
+        destination_path = app_settings.PHOTO_DIRECTORY + '/%s' % (filename)   
+        destination = open(destination_path, 'wb+')
+        for chunk in uploaded_file.chunks():
+            destination.write(chunk)
+        destination.close()
+        ENABLE_CELERY = getattr(app_settings, 'ENABLE_CELERY', defaults.ENABLE_CELERY)
+        if ENABLE_CELERY:
+            ThumbnailTask.delay(photo_new.id)
+
+        
+        return HttpResponse("ok", mimetype="text/plain")
+        
+    else:
+        
+        context['upload_dir'] = app_settings.PHOTO_DIRECTORY
+        context['album_slug'] = album_slug
+        context['location_slug'] = location_slug
+        context['domain_static'] = app_settings.DOMAIN_STATIC    
+        return render(request,'administrator/add_photos.html', context)
