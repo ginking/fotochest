c84bc7adb927fe1c3cd7f898b60d8fc663b33ec4
Derek Stegelman <dstegelman@gmail.com>
2012-12-16 18:55:34 -0600

    Check for image album first.

--- example/settings.py
+++ example/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.comments',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'fotochest.photo_manager',
