4697fd6c3b6722185b484067639d2a9cf754acc8
Derek Stegelman <dstegelman@gmail.com>
2014-07-23 21:59:11 -0600

    Factory object generation.

--- apps/photo_manager/tests/base.py
+++ apps/photo_manager/tests/base.py
@@ -1,17 +1,16 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
 
-from photo_manager.models import Photo, Album
 
-from locations.models import Location
+from photo_manager.tests.factories import PhotoFactory
 
 
 class BasePhotoTestClass(TestCase):
-
     def setUp(self):
-        self.user = User.objects.create(username='john', first_name='John', last_name='Doe')
-        self.location = Location.objects.create(city='Loveland', state='KS', country='USA', default_location=False)
-        self.album = Album.objects.create(user=self.user, title='New Album')
-        self.photo = Photo.objects.create(user=self.user, album=self.album, title='Hello World', location=self.location)
-        self.photo_2 = Photo.objects.create(user=self.user, album=self.album, title='second photo', location=self.location)
-        self.photo_3 = Photo.objects.create(user=self.user, album=self.album, title='third photo', location=self.location)
+
+        self.photo = PhotoFactory()
+        self.user = self.photo.user
+        self.album = self.photo.album
+        self.location = self.photo.location
+
+        self.photo_2 = PhotoFactory.create(user=self.user, album=self.album, title='second photo', location=self.location)
+        self.photo_3 = PhotoFactory.create(user=self.user, album=self.album, title='third photo', location=self.location)
