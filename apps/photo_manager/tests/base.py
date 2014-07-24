8c78ff40fafd05a61f9da8f50f25324516d72801
Derek Stegelman <dstegelman@gmail.com>
2014-07-23 20:47:44 -0600

    Moar tests.

--- apps/photo_manager/tests/base.py
+++ apps/photo_manager/tests/base.py
@@ -9,7 +9,9 @@ from locations.models import Location
 class BasePhotoTestClass(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create()
+        self.user = User.objects.create(username='john', first_name='John', last_name='Doe')
         self.location = Location.objects.create(city='Loveland', state='KS', country='USA', default_location=False)
         self.album = Album.objects.create(user=self.user, title='New Album')
-        self.photo = Photo.objects.create(album=self.album, title='Hello World', location=self.location)
+        self.photo = Photo.objects.create(user=self.user, album=self.album, title='Hello World', location=self.location)
+        self.photo_2 = Photo.objects.create(user=self.user, album=self.album, title='second photo', location=self.location)
+        self.photo_3 = Photo.objects.create(user=self.user, album=self.album, title='third photo', location=self.location)
--- apps/photo_manager/tests/test_models.py
+++ apps/photo_manager/tests/test_models.py
@@ -5,9 +5,7 @@ from .base import BasePhotoTestClass
 
 class AlbumModelTestCase(BasePhotoTestClass):
     def test_preview_photos(self):
-        p2 = Photo.objects.create(album=self.album, title='Hellow World Again')
-        self.assertIn(self.photo, self.album.preview_photos)
-        self.assertIn(p2, self.album.preview_photos)
+        self.assertIn(self.photo_2 or self.photo_3, self.album.preview_photos)
 
     def test_has_no_child_albums(self):
         self.assertFalse(self.album.has_child_albums)
@@ -18,12 +16,24 @@ class AlbumModelTestCase(BasePhotoTestClass):
         self.assertTrue(self.album.has_child_albums)
 
     def test_photo_count(self):
-        self.assertEqual(self.album.count, 1)
+        self.assertEqual(self.album.count, 3)
 
 
 class PhotoModelTestCase(BasePhotoTestClass):
 
     def test_photo_absolute_url(self):
         self.assertEqual(self.photo.get_absolute_url(), '/photos/%s/%s/' % (self.album.slug, self.photo.slug))
+
+    def test_get_next(self):
+        self.assertEqual(self.photo_2.get_next(), self.photo.get_absolute_url())
+
+    def test_get_previous(self):
+        self.assertEqual(self.photo_2.get_previous(), self.photo_3.get_absolute_url())
+
+    def test_author(self):
+        self.assertEqual(self.photo.author, 'John Doe')
+
+    def test_get_fullscreen_url(self):
+        self.assertEqual(self.photo.get_fullscreen(), '/photos/new-album/hello-world/fullscreen/')
 
 
